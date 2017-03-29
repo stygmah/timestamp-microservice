83624dee7ab24668a1fbc3c57cd54d62736a165f
Guillermo <stygmah@gmail.com>
2017-03-29 18:02:38 +0000

    First commit

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+
+const port = process.env.PORT || 3000;
+var app = express();
+app.use(bodyParser.json());
+
+
+app.post('/:date',(req,res)=>{
+  var date = req.date;
+  var newDate= new Date(date);
+  var returnDate = {
+    textDate: null,
+    unixTimeStamp: null
+  }
+  
+  //check if valid and then if unix timestamp or date text, convert after the
+  //missing one
+  if((newDate==="Invalid Date")){
+    if(isNaN(date)){
+      //text date
+      returnDate.textDate = date;
+      returnDate.unixTimeStamp = newDate.getTime()/1000;
+    }else{
+      //unix timestamp
+      returnDate.textDate = newDate;
+      returnDate.unixTimeStamp = date;
+    }
+  }
+  //send the response
+  res.send(JSON.stringify(returnDate));
+});
+
+
+
+app.listen(port, ()=>{
+  console.log('server listening on port ',port);
+});
+  
